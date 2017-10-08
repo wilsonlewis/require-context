2f44230a5a55b058e3b00bea3d4e78abf12b77b9
Wilson Lewis <wlewis@helloelephent.com>
2017-10-08 18:19:31 -0400

    Fix global registration

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
-if (!require.context) {
+module.exports = function(directory, recursive, regExp) {
   const dir = require('node-dir')
+  const path = require('path')
 
-  require.context = function(directory, recursive, regExp) {
-    const keys = dir
-      .files(directory, {
-        sync: true,
-        recursive: recursive || false
-      })
-      .filter(file => {
-        return file.match(regExp || /\.js$/)
-      })
+  const basepath =
+    directory[0] === '.'
+      ? path.resolve(__dirname + path.sep + directory)
+      : directory
 
-    const context = function(key) {
-      return require(key)
-    }
+  const keys = dir
+    .files(basepath, {
+      sync: true,
+      recursive: recursive || false
+    })
+    .filter(function(file) {
+      return file.match(regExp || /\.js$/)
+    })
+    .map(function(file) {
+      return file.slice(basepath.length + 1)
+    })
 
-    context.resolve = function(key) {
-      return key
-    }
+  const context = function(key) {
+    return require(context.resolve(key))
+  }
 
-    context.keys = function() {
-      return keys
-    }
+  context.resolve = function(key) {
+    return directory + path.sep + key
+  }
 
-    return context
+  context.keys = function() {
+    return keys
+  }
+
+  return context
+}
+
+module.exports.register = function() {
+  const wrap = module.constructor.wrap
+  module.constructor.wrap = function(script) {
+    return wrap(
+      'require.context = ' + module.exports.toString() + '\n' + script
+    )
   }
 }
